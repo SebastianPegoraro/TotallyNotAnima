274ee3b69d170924bf88abf612f613ecbc4a5b31
sebapego <sebapego18@gmail.com>
2020-04-18 22:22:25 -0300

    something

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var app = express();
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname + "/../public"));
 
 app.get('/', function (req, res) {
     res.redirect('index.html');
-})
+});
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'));
 
 var server = app.listen(5000, function () {
     console.log('Server running in port 5000');
-})
+});
